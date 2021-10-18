cdffe80f2b7a8e6dbad85f6e3590e0de61497e54
Miriam Valera <miriamvdaw@gmail.com>
2021-10-18 17:28:53 +0200

    add mapActions in incrementRandomInt

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -5,7 +5,7 @@
 
     <button @click="increment">+1</button>
     <button @click="incrementBy">+5</button>
-    <button @click="incrementRandomInt">Random</button>
+    <button @click="randomInt">Random</button>
 
     <h1>mapState</h1>
     <h2>mapState: {{ count }}</h2>
@@ -14,7 +14,7 @@
 
 <script>
 
-import { mapState } from 'vuex';
+import { mapState, mapActions } from 'vuex';
 
 export default {
 
@@ -25,7 +25,8 @@ export default {
         ...mapState(['count', 'lastMutation'])
         // ...mapState({
         //     count: state => state.count,
-        //     lastMutation: state => state.lastMutation
+        //     // lastMutation: state => state.lastMutation
+        //     lastMutation: 'lastMutation'
         // })
     },
     methods: {
@@ -35,11 +36,11 @@ export default {
         incrementBy() {
             this.$store.commit('incrementBy', 5) 
         },
-        incrementRandomInt() {
-            this.$store.dispatch('incrementRandomInt')
-        }
-        
-        
+
+        // ...mapActions(['incrementRandomInt'])  
+        ...mapActions({
+            randomInt: 'incrementRandomInt'
+        })  
     }
 
 }
